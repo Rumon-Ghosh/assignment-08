f5e26b5c3454f3ca38aadd58f1c417e35cd0af01
Rumon-Ghosh <rumon698@gmail.com>
2025-10-10 20:39:54 +0600

    styled footer again

--- src/assets/Components/Footer.jsx
+++ src/assets/Components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="footer footer-horizontal footer-center bg-gray-500 rounded text-white p-10">
+    <footer className="footer footer-horizontal footer-center bg-gray-500 text-white p-10">
       <nav>
         <div className="grid grid-flow-col gap-4">
           <a>
@@ -43,9 +43,13 @@ const Footer = () => {
       <nav className="grid grid-flow-col gap-4">
         <a className="link link-hover">About us</a>
         <a className="link link-hover">Contact</a>
-        <a className="link link-hover">Jobs</a>
-        <a className="link link-hover">Press kit</a>
+        <a className="link link-hover">Apps</a>
+        <a className="link link-hover">New Apps</a>
       </nav>
+      <div className="join">
+        <input className="input join-item text-black" placeholder="Email" />
+        <button className="btn join-item rounded-r-full">Subscribe</button>
+      </div>
       <aside>
         <p>
           Copyright © {new Date().getFullYear()} - All right reserved by Hero.io
