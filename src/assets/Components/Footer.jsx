34aeaae845572b2db7b165001f374afb71e06db8
Rumon-Ghosh <rumon698@gmail.com>
2025-10-10 17:13:25 +0600

    added the style of footer

--- src/assets/Components/Footer.jsx
+++ src/assets/Components/Footer.jsx
@@ -2,13 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="footer footer-horizontal footer-center bg-base-200 text-base-content rounded p-10">
-      <nav className="grid grid-flow-col gap-4">
-        <a className="link link-hover">About us</a>
-        <a className="link link-hover">Contact</a>
-        <a className="link link-hover">Jobs</a>
-        <a className="link link-hover">Press kit</a>
-      </nav>
+    <footer className="footer footer-horizontal footer-center bg-gray-500 rounded text-white p-10">
       <nav>
         <div className="grid grid-flow-col gap-4">
           <a>
@@ -45,6 +39,12 @@ const Footer = () => {
             </svg>
           </a>
         </div>
+      </nav>
+      <nav className="grid grid-flow-col gap-4">
+        <a className="link link-hover">About us</a>
+        <a className="link link-hover">Contact</a>
+        <a className="link link-hover">Jobs</a>
+        <a className="link link-hover">Press kit</a>
       </nav>
       <aside>
         <p>
